dc9e8a64d151f59b8823555ecec2410e403e41a2
masuyama <masuyama@synchro-food.co.jp>
2017-04-17 18:46:36 +0900

    セッション変数の暗号化 / RSpecの設定

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,14 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '91ed443f3249e64899d8a65ff843c143878e4a082fa635d87f2026de4774576dcef7ae3f6b167d3884ce75629b9e283293d3d6aa495d6ee541afbbed9552aebb'
+SampleApp::Application.config.secret_key_base = secure_token
